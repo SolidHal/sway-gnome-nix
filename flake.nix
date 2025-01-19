f8b3a97cbea7431e13e47f679358b57bd318ffc3
Eva Emmerich <eva@evaemmerich.com>
2025-01-19 14:58:31 -0600

    remove flake-utils

--- flake.nix
+++ flake.nix
@@ -1,26 +1,15 @@
 {
   description = "sway-gnome modules";
   inputs = {
-    flake-utils.url = "github:numtide/flake-utils";
-    nixpkgs.url = "github:coreyoconnor/nixpkgs/main";
   };
 
   outputs = {
     self,
-    flake-utils,
     nixpkgs,
   }:
     {
       nixosModules = {
         default = import ./default.nix;
       };
-    }
-    // flake-utils.lib.eachDefaultSystem (system: {
-      formatter = nixpkgs.legacyPackages.${system}.writeScriptBin "alejandra" ''
-        exec ${nixpkgs.legacyPackages.${system}.alejandra}/bin/alejandra \
-          --exclude ./dev-dependencies \
-          --exclude ./.git \
-          "$@"
-      '';
-    });
+    };
 }
