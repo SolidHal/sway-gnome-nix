52e77ad6dcebc372006e3686ac71ce9ce02e248c
Eva Emmerich <eva@evaemmerich.com>
2025-01-20 22:32:19 -0600

    remove seperate kanshi service
    regolith-displayd calls kanshi on its own

--- displayd.nix
+++ displayd.nix
@@ -1,4 +1,4 @@
-{ system, inputs, config, lib, pkgs, sway-gnome, ... }:
+{ system, inputs, config, lib, pkgs, ... }:
 
 with lib;
 
@@ -18,24 +18,6 @@ in {
       killall
     ];
 
-# https://github.com/regolith-linux/regolith-displayd/blob/96af6a84395930c03c0025891cf2c2553e963a00/debian/install
-#kanshi-init-service
-  systemd.user.services.regolith-init-kanshi = {
-    description = "Start Kanshi for Regolith";
-
-    partOf = [ "graphical-session.target" ];
-    wants = [ "gnome-session.target" ];
-    after = [ "gnome-session.target" ];
-
-    serviceConfig = {
-      Type = "simple";
-      Environment = "PATH=$PATH:${lib.makeBinPath [ pkgs.kanshi ]}" ;
-      ExecStart = "kanshi -c %h/.config/regolith3/kanshi/config";
-      Restart = "on-failure";
-      StartLimitIntervalSec = 10;
-      StartLimitBurst = 5;
-    };
-  };
 #regolith-init-displayd
   systemd.user.services.regolith-init-displayd = {
     description = "Start Regolith Display Daemon";
@@ -44,9 +26,6 @@ in {
     wants = [ "gnome-session.target" ];
     after = [ "gnome-session.target" ];
 
-    requires = [ "regolith-init-kanshi.service" ];
-    before = [ "regolith-init-kanshi.service" ];
-
     startLimitIntervalSec = 30;
     startLimitBurst = 5;
 
@@ -54,7 +33,7 @@ in {
       Type = "dbus";
       BusName = "org.gnome.Mutter.DisplayConfig";
       # TODO hardcoding the run path here is gross, figure out how to get regolith-displayd and regolith-displayd-init in the path like normal packages
-      Environment = "PATH=$PATH:${lib.makeBinPath [ pkgs.killall ]}:/run/current-system/sw/bin/";
+      Environment = "PATH=$PATH:${lib.makeBinPath [ pkgs.killall pkgs.kanshi ]}:/run/current-system/sw/bin/";
       ExecStartPre = "/run/current-system/sw/bin/regolith-displayd-init";
       ExecStart = "/run/current-system/sw/bin/regolith-displayd";
       Restart = "on-failure";
